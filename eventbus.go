f514569b2a1c7e8edf5aefcd5b607942bb59a73a
Mikkal Mcnulty <mikkal.r.mcnulty@medtronic.com>
2019-07-02 15:28:47 -0700

    make event bus a repo

---
+++ eventbus.go
@@ -0,0 +1,44 @@
+package eventbus
+
+import "sync"
+
+type DataEvent struct {
+	Data  interface{}
+	Topic string
+}
+
+type DataChannel chan DataEvent
+
+type DataChannelSlice []DataChannel
+
+type EventBus struct {
+	subscribers map[string]DataChannelSlice
+	rm          sync.RWMutex
+}
+
+func (eb *EventBus) Subscribe(topic string, ch DataChannel) {
+	eb.rm.Lock()
+
+	if prev, found := eb.subscribers[topic]; found {
+		eb.subscribers[topic] = append(prev, ch)
+	} else {
+		eb.subscribers[topic] = append([]DataChannel{}, ch)
+	}
+
+	eb.rm.Unlock()
+}
+
+func (eb *EventBus) Publish(topic string, data interface{}) {
+	eb.rm.RLock()
+
+	if chans, found := eb.subscribers[topic]; found {
+		channels := append(DataChannelSlice{}, chans...)
+		go func(data DataEvent, dataChannelSlices DataChannelSlice) {
+			for _, ch := range dataChannelSlices {
+				ch <- data
+			}
+		}(DataEvent{Data: data, Topic: topic}, channels)
+	}
+
+	eb.rm.RUnlock()
+}
